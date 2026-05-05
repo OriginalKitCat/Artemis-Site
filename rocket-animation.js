1cb298d9ce711bf52225080283adba730de5eaac
OriginalKitCat <originalkitcat@proton.me>
2026-05-05 19:48:16 +0200

    Working on the info cox below the rocket

--- rocket-animation.js
+++ rocket-animation.js
@@ -1,10 +1,21 @@
-const rocket = document.querySelector(".SLS-rocket");
+// const rocket = document.querySelector(".SLS-rocket");
 
-window.addEventListener("scroll", () => {
-    const triggerPoint = window.innerHeight * 0.5;
-    if (window.scrollY > triggerPoint) {
-        rocket.classList.add("fly");
-    } else {
-        rocket.classList.remove("fly");
-    }
+// window.addEventListener("scroll", () => {
+//     const triggerPoint = window.innerHeight * 0.5;
+//     if (window.scrollY > triggerPoint) {
+//         rocket.classList.add("fly");
+//     } else {
+//         rocket.classList.remove("fly");
+//     }
+// });
+
+document.addEventListener("DOMContentLoaded", () => {
+    const rocket = document.querySelector(".SLS-rocket");
+
+    window.addEventListener("scroll", () => {
+        const scrollY = window.scrollY;
+        const speed = 0.5;
+        const moveY = scrollY * speed;
+        rocket.style.transform = `translate(-50%, -${moveY}px)`;
+    });
 });
